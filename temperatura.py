1643c5e2215860f4fd3f95d89852b74124d13494
elput0amo <gmiradej@myuax.com>
2023-09-28 12:39:04 +0200

    Create temperatura.py

---
+++ temperatura.py
@@ -0,0 +1,4 @@
+temperatura_celcius = 5
+temperatura_farenheit= (temperatura_celcius*9/5)+32
+print ("Temperatura en Celsius: ", temperatura_celcius, )
+print ("Temperatura en Farenheit: ", temperatura_farenheit)
